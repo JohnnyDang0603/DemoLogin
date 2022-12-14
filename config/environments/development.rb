c240c8064793ce6dc24a6b4992c413959db932eb
jacky <jacky.le.goldenowl@gmail.com>
2022-12-14 22:32:57 +0700

    Add bullet config development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,19 +71,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   user_name: 'khoa.dp2k@gmail.com',
-  #   password: 'kbictpiqhxlylwaf',
-  #   domain: 'localhost:3000',
-  #   address: 'smtp.gmail.com',
-  #   port: '587',
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  # Set action mailer default url
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST'], port: 3000 }
+
+  # Letter Opener settings
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+  # Bullet settings
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
